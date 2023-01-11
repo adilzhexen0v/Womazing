8bdacbdca3014e6894973bad3ccb0497c30e7784
adilzhexenov <nescrypnoadil@gmail.com>
2023-01-11 10:44:41 +0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass')(require('sass'));
+const gulpSourcemaps = require('gulp-sourcemaps');
+
+gulp.task('styles-compile', () => {
+     return gulp.src('./app/scss/*.scss')
+     .pipe(gulpSourcemaps.init())
+     .pipe(gulpSass().on('error', gulpSass.logError))
+     .pipe(gulpSourcemaps.write('./'))
+     .pipe(gulp.dest('./dist/css/'));
+});
+
+
+const gulpWatch = require('gulp-watch');
+gulp.task('watch', () => {
+     gulp.watch('./app/scss/*.scss', gulp.series('styles-compile'));
+});
